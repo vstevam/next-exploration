4660e27b41adb984db591f003793862874fe1647
Vinicius Stevam Pereira Dias <vinicius.stevam@consensys.net>
2020-06-19 11:39:03 +0100

    fix load  timeline

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,7 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 export default class MyDocument extends Document {
+
   render() {
     return (
       <Html lang="en">
@@ -16,6 +17,27 @@ export default class MyDocument extends Document {
           <link rel="icon" type="image/png" href="assets/images/favicon.ico"/>
           {/* <!-- Google Analytics --> */}
           <script async src="https://www.googletagmanager.com/gtag/js?id=UA-119404237-1"></script>
+          {/* <!-- Scripts --> */}
+          <script defer src="//www.amcharts.com/lib/4/core.js"></script>
+          <script defer src="//www.amcharts.com/lib/4/charts.js"></script>
+          <script defer src="//www.amcharts.com/lib/4/maps.js"></script>
+          <script defer src="//www.amcharts.com/lib/4/plugins/forceDirected.js"></script>
+          <script defer src="//www.amcharts.com/lib/4/themes/dark.js"></script>
+          {/* <!-- Index --> */}
+          <script defer src="assets/js/jquery.min.js" ></script>
+          <script defer src="assets/js/jquery.scrolly.min.js" ></script>
+          <script defer src="assets/js/jquery.dropotron.min.js" ></script>
+          <script defer src="assets/js/jquery.scrollex.min.js" ></script>
+          <script defer src="assets/js/browser.min.js" ></script>
+          <script defer src="assets/js/breakpoints.min.js" ></script>
+          <script defer src="assets/js/util.js" ></script>
+          <script defer src="assets/js/main.js" ></script>
+          <script defer src="assets/src/profile.js" ></script>
+          {/* <!-- Resume --> */}
+          <script defer src="assets/js/amcharts.js" ></script>
+          <script defer src="assets/js/jquery-timeline.js" ></script>
+          <script defer src="assets/src/timeline.js" ></script>
+
         </Head>
         <body className="is-preload landing">
           <div className="page-wrapper">
@@ -23,25 +45,7 @@ export default class MyDocument extends Document {
             <NextScript />
 
           </div>
-            {/* <!-- Scripts --> */}
-          <script src="https://www.amcharts.com/lib/4/core.js"></script>
-          <script src="https://www.amcharts.com/lib/4/charts.js"></script>
-          <script src="https://www.amcharts.com/lib/4/plugins/forceDirected.js"></script>
-          <script src="https://www.amcharts.com/lib/4/themes/dark.js"></script>
-            {/* <!-- Index --> */}
-          <script src="assets/js/jquery.min.js"></script>
-          <script src="assets/js/jquery.scrolly.min.js"></script>
-          <script src="assets/js/jquery.dropotron.min.js"></script>
-          <script src="assets/js/jquery.scrollex.min.js"></script>
-          <script src="assets/js/browser.min.js"></script>
-          <script src="assets/js/breakpoints.min.js"></script>
-          <script src="assets/js/util.js"></script>
-          <script src="assets/js/main.js"></script>
-          <script src="assets/src/profile.js"></script>
-            {/* <!-- Resume --> */}
-          <script src="assets/js/amcharts.js"></script>
-          <script src="assets/js/jquery-timeline.js"></script>
-          <script src="assets/src/timeline.js"></script>
+
         </body>
       </Html>
     )
